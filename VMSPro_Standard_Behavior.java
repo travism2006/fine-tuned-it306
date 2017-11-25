519c6aabab0e3244cd7d74000e63b1c6f192eb8b
Travis Mitchum <travism2006@gmail.com>
2017-11-25 10:34:37 -0500

    New and Changed Files
    Eclipse could not push/commit for me. This is the manager DDC VMSPro with its used interfaces that contain various constants.

---
+++ VMSPro_Standard_Behavior.java
@@ -0,0 +1,12 @@
+package vmspro;
+
+/**
+ * Per documentation in the previous phase, this interface defines the required
+ * methods when implemeneted by the Customer and Vehicle DDCs.
+ * @author tmitchu2
+ * */
+public interface VMSPro_Standard_Behavior
+{
+	public String toString();
+	public boolean equals(Object obj);
+}
