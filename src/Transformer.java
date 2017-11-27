4ebd6916734b94d3713a226ba0e1bede5aaf3ab2
Hassan Ammar <hassan.ammar.07@gmail.com>
2017-11-26 20:37:58 -0500

    added the documentation, removed the methods that are implemented in the VMSPro.java file.

--- src/Transformer.java
+++ src/Transformer.java
@@ -2,25 +2,38 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+/**
+ * this class is designed for the purpose of sorting the object for displaying and reports purpose
+ * this class will have methods for different implementation.
+ * @author hassan ammar
+ *
+ */
 public class Transformer {
+	/**
+	 * this method will arrange the customer list in alphabetically by the first name
+	 * @param customerList
+	 * @return
+	 */
+	public static Set<Customer> sortCustomersByFistName(Set<Customer> customerList) {
 
-	// this class is going to be used to perform the business logic on the
-	// different object, for example customer
-
-	public static Customer findCustomerWithFirstAndLastName(List<Customer> customerList, String firstName,
-			String lastName) {
-
-		// return customer;
-		return null;
-	}
-
-	public static List<Customer> sortCustomersByFistName(List<Customer> customerList) {
-
-		Collections.sort(customerList, Comparator.comparing(Customer::getFirstName));
+		//Collections.sort(customerList, Comparator.comparing(Customer::getFirstName));
+		Set<Customer> sorted = new TreeSet<Customer>(new Comparator<Customer>() {
+            @Override
+            public int compare(Customer o1, Customer o2) {
+                return o2.compareTo(o1);
+            }
+        });
+		
+		sorted.
 
 		return customerList;
 	}
 
+	/**
+	 * this method will arrange the customer list in assending order by the customer id
+	 * @param customerList
+	 * @return
+	 */
 	public static List<Customer> sortCustomersById(List<Customer> customerList) {
 
 		Collections.sort(customerList, Comparator.comparing(Customer::getCustomerId));
@@ -28,6 +41,11 @@ public class Transformer {
 		return customerList;
 	}
 
+	/**
+	 * this method will arrange the customer list in alphabetically by the Last name
+	 * @param customerList
+	 * @return
+	 */
 	public static List<Customer> sortCustomersByLastName(List<Customer> customerList) {
 
 		Collections.sort(customerList, Comparator.comparing(Customer::getLastName));
@@ -35,20 +53,11 @@ public class Transformer {
 		return customerList;
 	}
 
-	public static List<Customer> addCustomer(List<Customer> customerList, Customer customer) {
-
-		customerList.add(customer);
-
-		return customerList;
-	}
-
-	public static List<Customer> removeCustomer(List<Customer> customerList, Customer customer) {
-
-		customerList.remove(customer);
-
-		return customerList;
-	}
-
+	/**
+	 * this method will arrange the vehicle list in  by the vin number
+	 * @param vehicleList
+	 * @return
+	 */
 	public static List<Vehicle> sortVehicleByVin(List<Vehicle> vehicleList) {
 
 		Collections.sort(vehicleList, Comparator.comparing(Vehicle::getVin));
@@ -56,6 +65,11 @@ public class Transformer {
 		return vehicleList;
 	}
 
+	/**
+	 * this method will arrange the vehicle list in alphabetically by the makers name
+	 * @param vehicleList
+	 * @return
+	 */
 	public static List<Vehicle> sortVehicleByMake(List<Vehicle> vehicleList) {
 
 		Collections.sort(vehicleList, Comparator.comparing(Vehicle::getMake));
@@ -63,6 +77,11 @@ public class Transformer {
 		return vehicleList;
 	}
 
+	/**
+	 * this method will arrange the vehicle list in alphabetically by the model
+	 * @param vehicleList
+	 * @return
+	 */
 	public static List<Vehicle> sortVehicleByModel(List<Vehicle> vehicleList) {
 
 		Collections.sort(vehicleList, Comparator.comparing(Vehicle::getModel));
