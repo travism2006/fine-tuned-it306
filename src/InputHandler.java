42eaa2985acaad1f3623fa9ced8ba3a7dd47c4b0
hassanammar07 <33819940+hassanammar07@users.noreply.github.com>
2017-12-03 02:43:10 -0500

    Update InputHandler.java
    adding the reading and writing to file for the vehicle object

--- src/InputHandler.java
+++ src/InputHandler.java
@@ -2,6 +2,8 @@
 * the class is designed for input and output to files
 * this class will read and write object to and from a file 
 * by converting them into json objects
+* 
+* @author Hassan Ammar
 */
 
 import java.io.BufferedInputStream;
@@ -21,11 +23,11 @@ import com.google.gson.Gson;
 
 public class InputHandler {
 
-	public static boolean writeToFile(List<Customer> customerList) {
+	public static boolean writeCustomerToFile(List<Customer> customerList) {
 
 		BufferedWriter bufferedWriter = null;
 		try {
-			File myFile = new File("MyTestFile.txt");
+			File myFile = new File("customer.txt");
 			if (!myFile.exists()) {
 				myFile.createNewFile();
 			}
@@ -56,13 +58,13 @@ public class InputHandler {
 	}
 
 	@SuppressWarnings("deprecation")
-	public static List<Customer> readToObject() {
+	public static List<Customer> readCustomerToObject() {
 		InputStream is = null;
 		BufferedInputStream bis = null;
 		DataInputStream dis = null;
 		List<Customer> returnCustomerList = new LinkedList<>();
 		try {
-			is = new FileInputStream("MyTestFile.txt");
+			is = new FileInputStream("customer.txt");
 			bis = new BufferedInputStream(is);
 			dis = new DataInputStream(bis);
 			String temp = null;
@@ -80,5 +82,73 @@ public class InputHandler {
 
 		return returnCustomerList;
 	}
-}
 
+	public static boolean writeVehicleToFile(List<Vehicle> vehicleList) {
+
+		BufferedWriter bufferedWriter = null;
+		try {
+			File myFile = new File("vehicle.txt");
+			if (!myFile.exists()) {
+				myFile.createNewFile();
+			}
+
+			Writer writer = new FileWriter(myFile);
+			bufferedWriter = new BufferedWriter(writer);
+
+			for (Vehicle veh : vehicleList) {
+
+				Gson g = new Gson();
+				String x = g.toJson(veh);
+				bufferedWriter.write(x);
+				bufferedWriter.newLine();
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (bufferedWriter != null)
+					bufferedWriter.close();
+			} catch (Exception ex) {
+
+			}
+		}
+
+		return true;
+	}
+
+	@SuppressWarnings("deprecation")
+	public static List<Vehicle> readVehicleToObject() {
+		InputStream is = null;
+		BufferedInputStream bis = null;
+		DataInputStream dis = null;
+		List<Vehicle> returnListVehicle = new LinkedList<>();
+		try {
+			is = new FileInputStream("vehicle.txt");
+			bis = new BufferedInputStream(is);
+			dis = new DataInputStream(bis);
+			String temp = null;
+			while ((temp = dis.readLine()) != null) {
+				Gson g = new Gson();
+				if (temp.contains("isConvertible")) {
+					Sedan s = g.fromJson(temp, Sedan.class);
+					returnListVehicle.add(s);
+				}
+				if (temp.contains("cargoSpace")) {
+					Van t = g.fromJson(temp, Van.class);
+					returnListVehicle.add(t);
+				}
+				if (temp.contains("towCapacity")) {
+					Van t = g.fromJson(temp, Van.class);
+					returnListVehicle.add(t);
+				}
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return returnListVehicle;
+	}
+}
